d6731f30357e8d3a1f3f14332def23f4a0ef793a
Mohit <mohit110891@gmail.com>
2015-06-03 00:24:11 +0530

    zoom

--- src/com/example/animation/MainActivity.java
+++ src/com/example/animation/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.animation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+	ImageView image ;
+	Button zoom;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		image = (ImageView)findViewById(R.id.image);
+		zoom = (Button)findViewById(R.id.zoom);
+		zoom.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				clockWise(getCurrentFocus());
+			}
+		});
+		
+		
+		
+		
+	}
+	
+	
+	public void clockWise(View view){
+		
+		Animation ani =  AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom);
+		image.setAnimation(ani);
+		
 	}
 
 }
